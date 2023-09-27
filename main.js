604d372ce0b7b2ed50dedb148a9a70b27f399edc
Riyagrrm <120349265+Riyagrrm@users.noreply.github.com>
2023-09-27 11:54:23 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+prediction_1 = ""
+prediction = ""
 Webcam.set({
     width: 350,
     height: 300,
@@ -18,4 +20,57 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 function modelLoaded()
 {
     console.log("modelLoaded")
+}
+function speak1()
+{
+    var synth = window.speechSynthesis
+    speak_data1 = "The first prediction is" + prediction_1;
+    speak_data2 = "The second prediction is" + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2)
+    synth.speak(utterThis)
+}
+function check()
+{
+    img = document.getElementById("rslt")
+    classifier.classify(img, gotResult)
+}
+function gotResult(error, result)
+{
+    if (error)
+    {
+        console.log(error)
+    }
+    else
+    {
+        console.log(result)
+        prediction_1 = result[0].label
+        prediction_2 = result[1].label
+        document.getElementById("result_gesture_name").innerHTML = prediction_1
+        document.getElementById("result_gesture_name2").innerHTML = prediction_2
+        speak1()
+        if (prediction_1 == "Good")
+        {
+            document.getElementById("result_ges").innerHTML = "&#128077"
+        }
+        if (prediction_1 == "Victory")
+        {
+            document.getElementById("result_ges").innerHTML = "&#9996"
+        }
+        if (prediction_1 == "Super")
+        {
+            document.getElementById("result_ges").innerHTML = "&#128076"
+        }
+        if (prediction_2 == "Good")
+        {
+            document.getElementById("result_ges2").innerHTML = "&#128077"
+        }
+        if (prediction_2 == "Victory")
+        {
+            document.getElementById("result_ges2").innerHTML = "&#9996"
+        }
+        if (prediction_2 == "Super")
+        {
+            document.getElementById("result_ges2").innerHTML = "&#128076"
+        }
+    }
 }
